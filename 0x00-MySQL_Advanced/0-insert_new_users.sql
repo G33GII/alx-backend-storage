f97a2e4506d41b43b4c049abb9ef5f6738936a9b
GEEGII <gazasolomon@gmail.com>
2024-08-07 17:08:03 +0100

    sql script - create user table in any DB

---
+++ 0x00-MySQL_Advanced/0-insert_new_users.sql
@@ -0,0 +1,10 @@
+-- Inserting New Users into ALX_backend - Users
+
+-- Inserting a new user
+-- INSERT INTO users(email, name) VALUE ("gazasolomon@gmail.com", "Gaza");
+INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
+INSERT INTO users (email, name) VALUES ("sylvie@dylan.com", "Sylvie");
+INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Jean");
+
+
+
