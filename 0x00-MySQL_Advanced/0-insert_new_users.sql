919261e10c6dc5997782d76654a6bb188d2eea62
GEEGII <gazasolomon@gmail.com>
2024-08-07 19:02:57 +0100

    update

--- 0x00-MySQL_Advanced/0-insert_new_users.sql
+++ 0x00-MySQL_Advanced/0-insert_new_users.sql
@@ -1,7 +1,7 @@
 -- Inserting New Users into ALX_backend - Users
 
 -- Inserting a new user
--- INSERT INTO users(email, name) VALUE ("gazasolomon@gmail.com", "Gaza");
+INSERT INTO users(email, name) VALUE ("gazasolomon@foo.com", "Gaza");
 INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
 INSERT INTO users (email, name) VALUES ("sylvie@dylan.com", "Sylvie");
 INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Jean");
