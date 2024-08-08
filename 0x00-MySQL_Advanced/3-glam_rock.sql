2dffcd3cea95b7ad6387dc58ed8b1015491cf9c8
GEEGII <gazasolomon@gmail.com>
2024-08-08 12:09:17 +0100

    filtering Glum bdnd and lifespan script

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- List Glam rock bands ranked by longevity
+SELECT band_name,
+    IFNULL(2022 - formed, 0) - IFNULL(2022 - split, 0) AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', style) > 0
+ORDER BY lifespan DESC,
+    band_name;
